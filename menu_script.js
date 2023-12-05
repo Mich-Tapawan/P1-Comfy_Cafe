37d0ef8ddf6767ab2f71483028da081c2499c7a4
Mich-Tapawan <mik.tapawan@gmail.com>
2023-12-05 14:11:57 +0800

    Finished the item category toggling function

--- menu_script.js
+++ menu_script.js
@@ -29,35 +29,48 @@ document.addEventListener("DOMContentLoaded", function () {
 
       }
     });
-
-    // Item category selection
-
-    // collect all the items and store in an array
-    // create a 2D list of sources for the different image categories imageSources = [categoryNum][looping through every string]
-    // create a loop that changes the image sources and h4 tag texts for each item
-
-    //let i = 0
-    // for (item in items){
-    //    item.style.image.src = imageSources[cat][i]
-    //    i++
-    //     }
-    let items = document.getElementsByClassName(items);
-    const imageSources = [['Media/cold-brew.jpg', 'Media/americano.png', 'Media/Espresso.png', 'Media/choco.png',
-                           'Media/cappuccino.png', 'Media/Latte.jpg', 'Media/mocha.png"', 'Media/matcha.png',
-                           'Media/affogato.png', 'Media/macchiato.png', 'Media/black.png', 'Media/caramel.png'],
-                          ['Media/'], [], [], [], [], [], [], []]
-    function changeCategory(category){
-      if ( category == "coffee"){
-        
-      }
-    }
-
-    document.getElementById("coffee").addEventListener("click",changeCategory("coffee"))
   });
 
+  // Hero section banners cycle
   let image = document.getElementById('main-tarp');;
   let images = ['Media/main-spread1.jpg','Media/main-spread2.jpg','Media/main-spread3.jpg']
   
   setInterval(function(){
   let random = Math.floor(Math.random() * images.length);
   image.src = images[random]}, 5000)
+
+  let items = document.getElementById('beverages').getElementsByTagName('img');
+  let labels = document.getElementById('beverages').getElementsByTagName('h4');
+  const imageSources = [['Media/cold-brew.jpg', 'Media/americano.png', 'Media/Espresso.png', 'Media/choco.png',
+                         'Media/cappuccino.png', 'Media/Latte.jpg', 'Media/mocha.png', 'Media/matcha.png',
+                         'Media/affogato.png', 'Media/macchiato.png', 'Media/black.png', 'Media/caramel.png'],
+                        ['Media/blackTea.webp', 'Media/greenTea.png', 'Media/matchaTea.jpg', 'Media/yellowTea.webp',
+                         'Media/jasmineTea.jpg', 'Media/', 'Media/', 'Media/',
+                         'Media/', 'Media/', 'Media/', 'Media/',],
+                          [], [], [], [], [], [], []]
+  
+  const itemLabels = [['COLD BREW', 'AMERICANO', 'ESPRESSO', 'HOT CHOCO',
+                       'CAPPUCCINO', 'LATTE', 'CAFE MOCHA', 'MATCHA',
+                       'AFFOGATO', 'MACCHIATO', 'LONG BLACK', 'CARAMEL'],
+                      ['BLACK TEA', 'GREEN TEA', 'MATCHA TEA', 'YELLOW TEA',
+                       'JASMINE TEA']]
+  
+  // Item category toggling
+  function changeCategory(category){
+    if ( category == "coffee"){
+      for (let i = 0; i < items.length; i++){
+        items[i].src = imageSources[0][i];
+        labels[i].textContent = itemLabels[0][i];
+      }
+    }
+
+    else if ( category == "tea"){
+      for (let i = 0; i < items.length; i++){
+        items[i].src = imageSources[1][i];
+        labels[i].textContent = itemLabels[1][i];  
+      }
+    }
+    }
+
+  document.getElementById('coffee').addEventListener('click', function() {changeCategory('coffee');});
+  document.getElementById('tea').addEventListener('click', function() {changeCategory('tea');});
