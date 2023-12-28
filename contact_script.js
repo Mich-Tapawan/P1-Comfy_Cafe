29252a6a22bcab41a97bfa17b1204b61d1983875
Mich-Tapawan <mik.tapawan@gmail.com>
2023-12-28 16:37:42 +0800

    Removed top nav function

--- contact_script.js
+++ contact_script.js
@@ -1,34 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Top nav
-    var navbar = document.getElementById("fixedNav");
-    var navName = document.getElementById("nav-name");
-    var navLogo = document.getElementById("logo");
-    var navLinks = document.getElementById("page-links");
-  
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 0) {
-        navbar.style.backgroundColor = "#3B2929";
-        navbar.style.height = "80px";
-        navLogo.style.width = "53px";
-        navLogo.style.height = "53px";
-        navLogo.style.left = "50px";
-        navName.style.fontSize = "30px";
-        navName.style.left = "40px";
-        navLinks.style.bottom = "5px";
-        navLinks.style.float = "null";
-      } 
-      else {
-        navbar.style.backgroundColor = "transparent";
-        navbar.style.height = "100px";
-        navLogo.style.width = "63px";
-        navLogo.style.height = "63px";
-        navLogo.style.left = "0px";
-        navName.style.fontSize = "35px";
-        navName.style.left = "0px";
-        navLinks.style.bottom = "0px";
-      }
-    });
-
     // Saving input from html form
     document.querySelector('form').onsubmit = function() {
         const name = document.querySelector('#name').value;
