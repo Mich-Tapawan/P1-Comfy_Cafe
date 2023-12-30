a471d94f04a510937caf51a073ce81bb05812bad
Mich-Tapawan <mik.tapawan@gmail.com>
2023-12-30 23:47:51 +0800

    Modified image sources array into key-value pairs

--- menu_script.js
+++ menu_script.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // Item category toggling
   let items = document.getElementById('beverages').getElementsByTagName('img');
   let labels = document.getElementById('beverages').getElementsByTagName('h4');
-  const imageSources = [['Media/cold-brew.jpg', 'Media/americano.png', 'Media/Espresso.png', 'Media/choco.png',
-                         'Media/cappuccino.png', 'Media/Latte.jpg', 'Media/mocha.png', 'Media/matcha.png',
-                         'Media/affogato.png', 'Media/macchiato.png', 'Media/black.png', 'Media/caramel.png'],
-                        ['Media/blackTea.webp', 'Media/greenTea.png', 'Media/matchaTea.jpg', 'Media/yellowTea.webp',
-                         'Media/jasmineTea.jpg', 'Media/peppermintTea.png', 'Media/oolongTea.jpg', 'Media/chaiTea.jpg',
-                         'Media/gingerTea.png', 'Media/redTea.png', 'Media/lemonTea.png', 'Media/hibiscusTea.png'],
+  const imageSources = [{coldBrew:'Media/cold-brew.jpg', americano:'Media/americano.png', espresso:'Media/Espresso.png', choco:'Media/choco.png',
+                         cappuccino:'Media/cappuccino.png', latte:'Media/Latte.jpg', mocha:'Media/mocha.png', matcha:'Media/matcha.png',
+                         affogato:'Media/affogato.png', macchiato:'Media/macchiato.png', black:'Media/black.png', caramel:'Media/caramel.png'},
+                        {blackTea:'Media/blackTea.webp', greenTea:'Media/greenTea.png', matchaTea:'Media/matchaTea.jpg', yellowTea:'Media/yellowTea.webp',
+                        jasmineTea:'Media/jasmineTea.jpg', peppermintTea:'Media/peppermintTea.png', oolongTea:'Media/oolongTea.jpg', chaiTea:'Media/chaiTea.jpg',
+                        gingerTea:'Media/gingerTea.png', redTea:'Media/redTea.png', lemonTea:'Media/lemonTea.png', hibiscusTea:'Media/hibiscusTea.png'},
                         [], [], [], [], [], [], []]
   
   const itemLabels = [['COLD BREW', 'AMERICANO', 'ESPRESSO', 'HOT CHOCO',
@@ -17,16 +17,19 @@ document.addEventListener("DOMContentLoaded", function () {
                        'JASMINE TEA', 'PEPPERMINT', 'OOLONG TEA', 'CHAI TEA',
                        'GINGER TEA', 'RED TEA', 'LEMON TEA', 'HIBISCUS TEA'],
                       [], [], [], [], [], [], []]
-  
+
   document.querySelectorAll('.category').forEach(function(category){
     category.onclick = function() {
       for (let i = 0; i < items.length; i++){
-        items[i].src = imageSources[category.dataset.index][i];
+        let current = imageSources[category.dataset.index]
+        let val = Object.values(current)
+        items[i].src = val[i];
         labels[i].textContent = itemLabels[category.dataset.index][i];
       }
     }
-  })
+  });
 
+  // Ordering Transaction Div - display and hide UI function
   if (document.querySelector('.order').style.display !== 'none'){
     document.querySelector('body').style.height = '100%';
     document.querySelector('body').style.overflowY = 'hidden';
