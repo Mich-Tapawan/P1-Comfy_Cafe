03cefa67b0991b462cd724f41dccdc0aab20401b
Mich-Tapawan <mik.tapawan@gmail.com>
2024-01-06 21:49:57 +0800

    Started total price computation for adding items

--- menu_script.js
+++ menu_script.js
@@ -2,30 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
   // Item category toggling
   let items = document.getElementById('beverages').getElementsByTagName('img');
   let labels = document.getElementById('beverages').getElementsByTagName('h4');
-  const imageSources = [[{source: 'Media/cold-brew.jpg', label:'COLD BREW', price: '', description:''},
-                        {source:'Media/americano.png', label:'AMERICANO', price: '', description:''},
-                        {source:'Media/Espresso.png', label:'ESPRESSO', price: '', description:''},
-                        {source:'Media/choco.png', label:'HOT CHOCO', price: '', description:''},
-                        {source:'Media/cappuccino.png', label:'CAPPUCCINO', price: '', description:''},
-                        {source:'Media/Latte.jpg', label:'LATTE', price: '', description:''},
-                        {source:'Media/mocha.png', label:'CAFE MOCHA', price: '', description:''},
-                        {source:'Media/matcha.png', label:'MATCHA', price: '', description:''},
-                        {source:'Media/affogato.png', label:'AFFOGATO', price: '', description:''},
-                        {source:'Media/macchiato.png', label:'MACCHIATO', price: '', description:''},
-                        {source:'Media/black.png', label:'LONG BLACK', price: '', description:''},
-                        {source:'Media/caramel.png', label:'CARAMEL', price: '', description:''}],
-                        [{source:'Media/blackTea.webp', label:'BLACK TEA', price: '', description:''},
-                        {source:'Media/greenTea.png', label:'GREEN TEA', price: '', description:''},
-                        {source:'Media/matchaTea.jpg', label:'MATCHA TEA', price: '', description:''},
-                        {source:'Media/yellowTea.webp', label:'YELLOW TEA', price: '', description:''},
-                        {source:'Media/jasmineTea.jpg', label:'JASMINE TEA', price: '', description:''},
-                        {source:'Media/peppermintTea.png', label:'PEPPERMINT TEA', price: '', description:''},
-                        {source:'Media/oolongTea.jpg', label:'OOLONG TEA', price: '', description:''},
-                        {source:'Media/chaiTea.jpg', label:'CHAI TEA', price: '', description:''},
-                        {source:'Media/gingerTea.png', label:'GINGER TEA', price: '', description:''},
-                        {source:'Media/redTea.png', label:'RED TEA', price: '', description:''},
-                        {source:'Media/lemonTea.png', label:'LEMON TEA', price: '', description:''},
-                        {source:'Media/hibiscusTea.png', label:'HIBISCUS TEA', price: '', description:''}],
+  const imageSources = [[{source: 'Media/cold-brew.jpg', label:'COLD BREW', price: 5, description:''},
+                        {source:'Media/americano.png', label:'AMERICANO', price: 5, description:''},
+                        {source:'Media/Espresso.png', label:'ESPRESSO', price: 3, description:''},
+                        {source:'Media/choco.png', label:'HOT CHOCO', price: 7, description:''},
+                        {source:'Media/cappuccino.png', label:'CAPPUCCINO', price: 7, description:''},
+                        {source:'Media/Latte.jpg', label:'LATTE', price: 6, description:''},
+                        {source:'Media/mocha.png', label:'CAFE MOCHA', price: 6, description:''},
+                        {source:'Media/matcha.png', label:'MATCHA', price: 7, description:''},
+                        {source:'Media/affogato.png', label:'AFFOGATO', price: 7, description:''},
+                        {source:'Media/macchiato.png', label:'MACCHIATO', price: 7, description:''},
+                        {source:'Media/black.png', label:'LONG BLACK', price: 5, description:''},
+                        {source:'Media/caramel.png', label:'CARAMEL', price: 6, description:''}],
+                        [{source:'Media/blackTea.webp', label:'BLACK TEA', price: 5, description:''},
+                        {source:'Media/greenTea.png', label:'GREEN TEA', price: 3, description:''},
+                        {source:'Media/matchaTea.jpg', label:'MATCHA TEA', price: 7, description:''},
+                        {source:'Media/yellowTea.webp', label:'YELLOW TEA', price: 4, description:''},
+                        {source:'Media/jasmineTea.jpg', label:'JASMINE TEA', price: 4, description:''},
+                        {source:'Media/peppermintTea.png', label:'PEPPERMINT TEA', price: 5, description:''},
+                        {source:'Media/oolongTea.jpg', label:'OOLONG TEA', price: 6, description:''},
+                        {source:'Media/chaiTea.jpg', label:'CHAI TEA', price: 5, description:''},
+                        {source:'Media/gingerTea.png', label:'GINGER TEA', price: 3, description:''},
+                        {source:'Media/redTea.png', label:'RED TEA', price: 5, description:''},
+                        {source:'Media/lemonTea.png', label:'LEMON TEA', price: 4, description:''},
+                        {source:'Media/hibiscusTea.png', label:'HIBISCUS TEA', price: 7, description:''}],
                         [], [], [], [], [], [], []]
 
   let categoryIndex = 0;
@@ -50,6 +50,47 @@ document.addEventListener("DOMContentLoaded", function () {
       imagePlaceholder.src = currentItem.source;
       textPlaceholder.textContent = currentItem.label;
 
+
+
+
+      // Amount toggle
+      let amount = document.getElementById('amount');
+      let totalText = document.getElementById('total');
+      var count = 1;
+      var total = currentItem.price;
+
+      // Drink Sizes Percentage
+      let chosenSize = document.querySelector('input[name="size"]:checked');
+      let addedPrice = 1.2;
+      total *= addedPrice;
+      totalText.textContent = `Total: ${total.toFixed(2)} USD`;
+      
+      if(chosenSize === 'medium'){
+        addedPrice = 1.2;
+      }
+      else if(chosenSize === 'large'){
+        addedPrice = 1.5;
+      }
+
+      document.querySelector('#minus').addEventListener('click', () => {
+        if(count > 1){
+          count--;
+          amount.value = `Amount: ${count}`;
+          total -= currentItem.price;
+          total *= addedPrice;
+          totalText.textContent = `Total: ${total.toFixed(2)} USD`;
+        }
+      });
+      document.querySelector('#plus').addEventListener('click', () => {
+        if(count < 20){
+          count++;
+          amount.value = `Amount: ${count}`;
+          total += currentItem.price;
+          total *= addedPrice;
+          totalText.textContent = `Total: ${total.toFixed(2)} USD`;
+        }
+      });
+
       document.querySelector('.order').style.display = 'block';
       document.querySelector('body').style.height = '100%';
       document.querySelector('body').style.overflowY = 'hidden';
@@ -63,5 +104,5 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector('.container').style.webkitFilter = "blur(0px)";
       }
     }
-  }) 
+  }); 
 });
