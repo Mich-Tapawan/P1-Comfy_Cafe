4e45123de4669b44f8b8f84086bce97102adceea
Mich-Tapawan <mik.tapawan@gmail.com>
2024-01-11 23:32:15 +0800

    initial

--- main_script.js
+++ main_script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Hero Section banner images cycle
-  let image = document.getElementById('main-tarp');;
+  let image = document.getElementById('main-tarp');
   let images = ['Media/main-spread1.jpg','Media/main-spread2.jpg','Media/main-spread3.jpg']
   
   setInterval(function(){
