3936cecc0ca30082edfc173fe3a03a548fe3cf17
Mich-Tapawan <mik.tapawan@gmail.com>
2024-01-14 22:22:20 +0800

    Finished automatic image slider loop

--- main_script.js
+++ main_script.js
@@ -1,11 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Hero Section banner images cycle
   let image = document.getElementById('main-tarp');
+  let hero = document.querySelector('.hero');
   let images = ['Media/main-spread1.jpg','Media/main-spread2.jpg','Media/main-spread3.jpg'];
-  
+  let count = 0;
+  let clicked = false;
+
+  hero.onmouseover = () => {
+      document.querySelectorAll('.slideButton').forEach(button =>{
+      button.style.display = 'block';
+    })
+  }
+
+  hero.onmouseleave = () => {
+      document.querySelectorAll('.slideButton').forEach(button =>{
+      button.style.display = 'none';
+    })
+  }
+
   setInterval(function(){
-    let random = Math.floor(Math.random() * images.length);
-    image.src = images[random]}, 5000)
+    //let random = Math.floor(Math.random() * images.length);
+    if(count === images.length){
+      count = 0;
+    }
+    image.src = images[count];
+    count++}, 5000)
 
   document.querySelectorAll('.slideButton').forEach(button => {
     button.onclick = () =>{
