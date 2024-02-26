f44d5e393736c084cf0c63d2204466e87b7b86b2
Mich-Tapawan <mik.tapawan@gmail.com>
2024-02-26 12:26:33 +0800

    repositioned nav elements in different media widths

--- nav.js
+++ nav.js
@@ -2,30 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
     // Top nav
     var navbar = document.getElementById("fixedNav");
     var navName = document.getElementById("nav-name");
+    var nameStyle = getComputedStyle(navName);
     var navLogo = document.getElementById("logo");
+    var logoStyle = getComputedStyle(navLogo);
     var navLinks = document.getElementById("page-links");
+    var linkStyle = getComputedStyle(navLinks);
   
     window.addEventListener("scroll", function () {
       if (window.scrollY > 0) {
         navbar.style.backgroundColor = "#3B2929";
         navbar.style.height = "80px";
-        navLogo.style.width = "53px";
-        navLogo.style.height = "53px";
-        navLogo.style.left = "50px";
-        navName.style.fontSize = "30px";
-        navName.style.left = "40px";
+        navLogo.style.width = logoStyle.width - 10;
+        navLogo.style.height = logoStyle.height - 10;;
+        navName.style.fontSize = nameStyle.fontSize - 5;
+        navLogo.style.left = '40px';
+        navName.style.left = '40px';
+        navLogo.style.bottom = '5px';
         navLinks.style.bottom = "5px";
+        navLinks.style.transform = 'translateX(-48px)';
         navLinks.style.float = "null";
       } 
       else {
         navbar.style.backgroundColor = "transparent";
         navbar.style.height = "100px";
-        navLogo.style.width = "63px";
-        navLogo.style.height = "63px";
-        navLogo.style.left = "0px";
-        navName.style.fontSize = "35px";
-        navName.style.left = "0px";
+        navLogo.style.width = logoStyle.width;
+        navLogo.style.height = logoStyle.height;
+        navName.style.fontSize = nameStyle.fontSize;
         navLinks.style.bottom = "0px";
+        navLogo.style.left = '0px';
+        navName.style.left = '0px';
+        navLinks.style.transform = '0px';
+        navLinks.style.transition = '0.3s ease';
       }
     });
 });
